2a23daefcf47e0da47bd9d0a52117c001c735b58
Glutinous-rice <1984947356@qq.com>
2019-07-26 12:23:41 +0800

    增加注释

--- 窗口坐标系.py
+++ 窗口坐标系.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication,QMainWindow,QPushButton,QHBoxLayout,QWidget
-
+#定义一个槽
 def on_Click():
     print('1')
     print('widget.x()=%d' % widget.x())
@@ -25,7 +25,7 @@ widget=QWidget()            #创建一个窗口
 btn=QPushButton(widget)     #创建一个按钮，继承widget的类
 btn.setText('按钮')         #设置button的文字
 btn.move(24,52)             #设置button的位置
-btn.clicked.connect(on_Click)
+btn.clicked.connect(on_Click) #建立槽与信号的关联
 widget.resize(300,240)      #设置窗口的大小
 widget.move(250,200)
 widget.setWindowTitle('屏幕坐标系')
